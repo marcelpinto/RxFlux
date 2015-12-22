1ff34c122db02985d1e9806e362194f9e11f7595
AndreiD <danandreist@gmail.com>
2015-12-22 17:14:06 +0200

    switched to retrofit 2.0. added sample proguard rules.

--- app/src/main/java/com/hardsoftstudio/rxflux/sample/core/GitHubApi.java
+++ app/src/main/java/com/hardsoftstudio/rxflux/sample/core/GitHubApi.java
@@ -2,21 +2,51 @@ package com.hardsoftstudio.rxflux.sample.core;
 
 import com.hardsoftstudio.rxflux.sample.model.GitHubRepo;
 import com.hardsoftstudio.rxflux.sample.model.GitUser;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.logging.HttpLoggingInterceptor;
+
 import java.util.ArrayList;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import rx.Observable;
 
-/**
- * Created by marcel on 09/10/15.
- */
 public interface GitHubApi {
 
-  String SERVICE_ENDPOINT = "https://api.github.com";
+  String ENDPOINT = "https://api.github.com";
 
-  @GET("/repositories")
-  Observable<ArrayList<GitHubRepo>> getRepositories();
+  @GET("/repositories") Observable<ArrayList<GitHubRepo>> getRepositories();
 
-  @GET("/users/{id}")
-  Observable<GitUser> getUser(@Path("id") String userId);
+  @GET("/users/{id}") Observable<GitUser> getUser(@Path("id") String userId);
+
+  class Factory {
+    private static GitHubApi instance;
+
+    private static void create() {
+
+      //------- enable logging ------
+      OkHttpClient client = new OkHttpClient();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+      //need custom headers ?
+      //client.interceptors().add(interceptor);
+      //client.networkInterceptors().add(chain -> {
+      //  Request request = chain.request().newBuilder().addHeader("Authorization", authToken).build();
+      //  return chain.proceed(request);
+      //});
+
+      final Retrofit retrofit = new Retrofit.Builder().baseUrl(GitHubApi.ENDPOINT).client(client).addConverterFactory(GsonConverterFactory.create()).addCallAdapterFactory(RxJavaCallAdapterFactory.create()).build();
+      instance = retrofit.create(GitHubApi.class);
+    }
+
+    public static synchronized GitHubApi getApi() {
+      if (instance == null) {
+        create();
+      }
+      return instance;
+    }
+  }
 }
